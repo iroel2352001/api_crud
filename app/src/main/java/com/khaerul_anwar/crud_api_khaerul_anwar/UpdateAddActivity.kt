a6011d85225b387f933a4d4a9e7e8fab261e80be
iroel2352001 <khaerul_23520011@stimata.ac.id>
2024-06-28 19:29:25 +0800

    Add files via upload

---
+++ app/src/main/java/com/khaerul_anwar/crud_api_khaerul_anwar/UpdateAddActivity.kt
@@ -0,0 +1,20 @@
+package com.khaerul_anwar.crud_api_khaerul_anwar
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UpdateAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_update_add2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
